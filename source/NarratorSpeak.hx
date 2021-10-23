83204540b17e9e28ec071622f9d19b344bac1899
Diaconu Bogdan <diaconubogdan2016@gmail.com>
2021-10-23 13:42:29 +0300

    Enabled antialiasing on NarratorSpeak texts.

--- source/NarratorSpeak.hx
+++ source/NarratorSpeak.hx
@@ -25,10 +25,12 @@ class NarratorSpeak extends FlxSpriteGroup
 		typeText = new FlxTypeText(0, 300, 500, lines[curPart]);
 		typeText.setFormat('assets/data/fonts/karma.TTF', 40, FlxColor.BLACK, CENTER);
 		typeText.screenCenter(X);
+		typeText.antialiasing = true;
 
 		doneText = new FlxText(0, typeText.y - 25, "Press ENTER to continue");
 		doneText.setFormat('assets/data/fonts/karma.TTF', 20, FlxColor.BLACK, CENTER);
 		doneText.screenCenter(X);
+		doneText.antialiasing = true;
 		add(doneText);
 		// typeText.sounds = [FlxG.sound.load('assets/sounds/dialogBeep.wav')];
 		add(typeText);
