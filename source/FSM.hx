d17c51f97b9ddd1b1eacc621a8ddd368e811dc0e
Diaconu Bogdan <diaconubogdan2016@gmail.com>
2022-01-19 12:48:33 +0200

    Made an FSM class.

---
+++ source/FSM.hx
@@ -0,0 +1,12 @@
+package;
+
+class FSM
+{
+	public var activeState:Float->Void;
+
+	public function new(initialState:Float->Void)
+		activeState = initialState;
+
+	public function update(elapsed:Float)
+		activeState(elapsed);
+}
