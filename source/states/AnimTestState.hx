c7023b0922e56054e3b1d52a5b230abf17c23ac7
Diaconu Bogdan <diaconubogdan2016@gmail.com>
2022-01-28 11:57:31 +0200

    Updated AnimTestState to the latest version.

--- source/states/AnimTestState.hx
+++ source/states/AnimTestState.hx
@@ -1,22 +1,28 @@
 package;
 
+import flixel.FlxCamera;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.addons.display.FlxBackdrop;
 import flixel.addons.display.FlxGridOverlay;
 import flixel.addons.ui.FlxInputText;
+import flixel.addons.ui.FlxUICheckBox;
 import flixel.addons.ui.FlxUIDropDownMenu;
+import flixel.addons.ui.FlxUINumericStepper;
 import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.text.FlxText;
+import flixel.tweens.FlxTween;
 import flixel.ui.FlxButton;
 import flixel.util.FlxColor;
-import flixel.util.FlxSave;
+import flixel.util.FlxTimer;
 import haxe.Json;
 import lime.utils.Assets;
 
+using StringTools;
+
 /*
- * Animation Debug Test (I'm proud of this :D).
- * This was made in a rush so pls cut me some slack D: (Making the code look nicer is on my to do list :P)
  * Developed by Bogdan2D.
  */
 typedef HitboxData =
@@ -28,98 +34,38 @@ typedef HitboxData =
 	scale:Float
 }
 
+//-----[PATH SETUP]-----\\
+var path_charList:String = 'assets/data/charList.txt';
+var path_offset:String = 'assets/data/offsets';
+var path_charFolder:String = 'assets/images';
+
+//-----------------------\\
+
 class AnimTestState extends FlxState
 {
 	var displayChar:FlxSprite;
+	var displayHitbox:FlxSprite;
 
-	var charList:Array<String> = TextUtil.arrayifyTextFile('assets/data/charList.txt');
+	var charList:Array<String> = arrayifyTextFile(path_charList);
 	var charDropdown:FlxUIDropDownMenu;
 	var animInput:FlxInputText;
 	var playAnimBTN:FlxButton;
 
 	var loadHitboxBTN:FlxButton;
-	var arrayHitboxChoice:FlxInputText;
+	var arrayHitboxChoice:FlxUINumericStepper;
+
+	var antialiasingEnabled:FlxUICheckBox;
 
 	var moveSpeed:Int;
 	var daBoxData:HitboxData;
 
 	// DataLol
 	var offsetDisplayText:FlxText;
+	// Cameras
+	var prevCam:FlxCamera;
+	var uiCam:FlxCamera;
 
-	override public function create()
-	{
-		FlxG.sound.muteKeys = null; // >:(
-		FlxG.debugger.drawDebug = true;
-		FlxG.mouse.visible = true;
-
-		var bg:FlxSprite = FlxGridOverlay.create(50, 50);
-		add(bg);
-
-		charDropdown = new FlxUIDropDownMenu(10, 10, FlxUIDropDownMenu.makeStrIdLabelArray(charList, true));
-		animInput = new FlxInputText(137, 10, 250, 'idle', 15, FlxColor.BLACK, FlxColor.WHITE);
-		playAnimBTN = new FlxButton(400, 10, 'Play', playAnim);
-		loadHitboxBTN = new FlxButton(1140, 697, 'Load HITBOX', updateDaHitbox);
-		arrayHitboxChoice = new FlxInputText(1225, 696, 50, '0', 15, FlxColor.BLACK, FlxColor.WHITE);
-
-		offsetDisplayText = new FlxText(0, 0, FlxG.width);
-		offsetDisplayText.setFormat(null, 30, FlxColor.BLACK, FlxTextAlign.RIGHT);
-
-		//-----[CHARACTER]-----\\
-		displayChar = new FlxSprite();
-		// displayChar.antialiasing = true;
-		playAnim();
-		FlxG.debugger.visible = false;
-		displayChar.screenCenter();
-
-		// Layering
-		add(displayChar);
-
-		add(charDropdown);
-		add(animInput);
-		add(playAnimBTN);
-		add(offsetDisplayText);
-		add(loadHitboxBTN);
-		add(arrayHitboxChoice);
-
-		super.create();
-		displayChar.frames = sparrowFrames(charDropdown.selectedLabel);
-	}
-
-	override public function update(elapsed)
-	{
-		// This is a disaster lol
-		super.update(elapsed);
-		hitboxControl();
-		charDrag();
-
-		// Control basically
-		if (!animInput.hasFocus && !arrayHitboxChoice.hasFocus)
-		{
-			if (FlxG.keys.justPressed.F)
-				FlxG.fullscreen = !FlxG.fullscreen;
-
-			if (FlxG.keys.justPressed.SPACE)
-				playAnim(); // quick play :)
-
-			if (FlxG.keys.justPressed.Q)
-				FlxG.debugger.drawDebug = !FlxG.debugger.drawDebug;
-
-			if (FlxG.keys.justPressed.ENTER)
-				displayChar.screenCenter();
-
-			if (FlxG.keys.justPressed.K)
-				displayChar.flipX = !displayChar.flipX;
-		}
-
-		if (FlxG.keys.pressed.SHIFT)
-			moveSpeed = 5;
-		else
-			moveSpeed = 1;
-
-		// INFO TEXT
-		offsetDisplayText.text = 'HITBOX DATA\nX: ' + displayChar.offset.x + '\nY:' + displayChar.offset.y + '\nHEIGHT:' + displayChar.height + '\nWIDTH:'
-			+ displayChar.width;
-	}
+	var bg:FlxSprite;
 
 	function playAnim()
 	{
@@ -144,9 +90,154 @@ class AnimTestState extends FlxState
 		}
 	}
 
+	override public function create()
+	{
+		FlxG.resizeWindow(1280, 720);
+		FlxG.fixedTimestep = false; // If you want things like replays in yo game make sure to delete this line.
+		// FlxG.sound.muteKeys = null; // >:(
+		// FlxG.debugger.drawDebug = true;
+		FlxG.mouse.visible = true;
+
+		//-----[Camera crap]-----\\
+		prevCam = new FlxCamera();
+		uiCam = new FlxCamera();
+
+		FlxG.cameras.reset(prevCam);
+		FlxG.cameras.add(uiCam);
+		FlxCamera.defaultCameras = [prevCam];
+
+		uiCam.bgColor.alpha = 0;
+
+		bg = FlxGridOverlay.create(48, 48);
+		bg.scrollFactor.set(0, 0);
+		add(bg);
+		// var repatShit:FlxBackdrop = new FlxBackdrop(bg, 1, 1);
+
+		charDropdown = new FlxUIDropDownMenu(10, 10, FlxUIDropDownMenu.makeStrIdLabelArray(charList, true));
+		animInput = new FlxInputText(137, 10, 250, 'idle', 15, FlxColor.BLACK, FlxColor.WHITE);
+		playAnimBTN = new FlxButton(400, 10, 'Play', playAnim);
+		loadHitboxBTN = new FlxButton(1137, 697, 'Load HITBOX', updateDaHitbox);
+		arrayHitboxChoice = new FlxUINumericStepper(1220, 700, 1, 0, 0);
+		antialiasingEnabled = new FlxUICheckBox(10, 700, null, null, "Antialiasing", 100, function() displayChar.antialiasing = antialiasingEnabled.checked);
+		antialiasingEnabled.textY -= 3;
+
+		offsetDisplayText = new FlxText(0, 0, FlxG.width);
+		offsetDisplayText.setFormat(null, 30, FlxColor.BLACK, FlxTextAlign.RIGHT);
+
+		displayHitbox = new FlxSprite();
+		displayHitbox.makeGraphic(10, 10, FlxColor.RED);
+		displayHitbox.alpha = 0.5;
+
+		//-----[CHARACTER]-----\\
+		displayChar = new FlxSprite();
+		playAnim();
+		FlxG.debugger.visible = false;
+		displayChar.screenCenter();
+
+		// Layering
+		add(displayChar);
+		add(displayHitbox);
+
+		// UI
+		add(charDropdown);
+		charDropdown.cameras = [uiCam];
+		add(animInput);
+		animInput.cameras = [uiCam];
+		add(playAnimBTN);
+		playAnimBTN.cameras = [uiCam];
+		add(offsetDisplayText);
+		offsetDisplayText.cameras = [uiCam];
+		add(loadHitboxBTN);
+		loadHitboxBTN.cameras = [uiCam];
+		add(arrayHitboxChoice);
+		arrayHitboxChoice.cameras = [uiCam];
+		add(antialiasingEnabled);
+		antialiasingEnabled.cameras = [uiCam];
+
+		//-------------\\
+		super.create();
+		prevCam.bgColor = FlxColor.BLACK;
+		//-------------\\
+		displayChar.frames = sparrowFrames(charDropdown.selectedLabel);
+
+		var HelpText:FlxText = new FlxText(10, 660, FlxG.width, "Press F1 for help.");
+		HelpText.setFormat(null, 30, FlxColor.BLACK, LEFT);
+		add(HelpText);
+		new FlxTimer().start(5, function(tmr:FlxTimer)
+		{
+			FlxTween.tween(HelpText, {alpha: 0}, 1, {
+				onComplete: function(twn:FlxTween)
+				{
+					remove(HelpText);
+				}
+			});
+		});
+	}
+
+	override public function update(elapsed)
+	{
+		// This is a disaster lol
+		super.update(elapsed);
+		hitboxControl();
+		charDrag();
+
+		if (!animInput.hasFocus)
+		{
+			if (FlxG.keys.justPressed.F)
+				FlxG.fullscreen = !FlxG.fullscreen;
+
+			if (FlxG.keys.justPressed.SPACE)
+				playAnim(); // quick play :)
+
+			if (FlxG.keys.justPressed.Q)
+				displayHitbox.visible = !displayHitbox.visible;
+			// FlxG.debugger.drawDebug = !FlxG.debugger.drawDebug;
+
+			if (FlxG.keys.justPressed.ENTER)
+				displayChar.screenCenter();
+
+			if (FlxG.keys.justPressed.K)
+				displayChar.flipX = !displayChar.flipX;
+
+			if (FlxG.keys.justPressed.R)
+				displayChar.updateHitbox();
+
+			if (FlxG.keys.justPressed.F1)
+				FlxG.openURL('https://github.com/Bogdan2D/FlixelSparrowAnimationDebug/wiki/3.-How-to-use.');
+
+			// CAMERA MOVEMENT
+			if (FlxG.keys.pressed.X)
+				prevCam.zoom += 0.05;
+
+			if (FlxG.keys.pressed.Z && prevCam.zoom > 1)
+				prevCam.zoom -= 0.05;
+		}
+
+		if (FlxG.keys.pressed.SHIFT)
+			moveSpeed = 5;
+		else
+			moveSpeed = 1;
+
+		if (FlxG.keys.justPressed.NINE)
+		{
+			if (bg.alpha == 1)
+				bg.alpha = 0.5;
+			else
+				bg.alpha = 1;
+		}
+		displayHitbox.setPosition(displayChar.x, displayChar.y);
+		displayHitbox.makeGraphic(Std.parseInt('' + displayChar.width), Std.parseInt('' + displayChar.height), FlxColor.RED);
+		// ^^ lol this thing actually worked :)))
+		displayHitbox.updateHitbox();
+
+		// INFO TEXT
+		offsetDisplayText.text = 'HITBOX DATA\nX: ' + displayChar.offset.x + '\nY:' + displayChar.offset.y + '\nHEIGHT:' + displayChar.height + '\nWIDTH:'
+			+ displayChar.width + '\n SPRITE SCALE\n' + displayChar.scale.x;
+	}
+
 	function hitboxControl()
 	{
-		if (!animInput.hasFocus && !arrayHitboxChoice.hasFocus)
+		if (!animInput.hasFocus)
 		{
 			if (moveSpeed < 2)
 			{
@@ -170,6 +261,20 @@ class AnimTestState extends FlxState
 				if (FlxG.keys.justPressed.S)
 					displayChar.height += 1;
 			}
+			if (FlxG.keys.justPressed.P)
+			{
+				if (moveSpeed == 1)
+					displayChar.scale.add(0.01, 0.01);
+				else
+					displayChar.scale.add(0.1, 0.1);
+			}
+			if (FlxG.keys.justPressed.O)
+			{
+				if (moveSpeed == 1)
+					displayChar.scale.add(-0.01, -0.01);
+				else
+					displayChar.scale.add(-0.1, -0.1);
+			}
 		}
 	}
 
@@ -179,23 +284,20 @@ class AnimTestState extends FlxState
 			if (FlxG.mouse.pressed)
 				displayChar.setPosition(FlxG.mouse.getPosition().x - displayChar.width / 2, FlxG.mouse.getPosition().y - displayChar.height / 2);
 
-		if (!animInput.hasFocus && !arrayHitboxChoice.hasFocus)
-		{
-			if (FlxG.keys.pressed.UP)
-				displayChar.y -= moveSpeed;
-			if (FlxG.keys.pressed.DOWN)
-				displayChar.y += moveSpeed;
-			if (FlxG.keys.pressed.LEFT)
-				displayChar.x -= moveSpeed;
-			if (FlxG.keys.pressed.RIGHT)
-				displayChar.x += moveSpeed;
-		}
+		if (FlxG.keys.pressed.UP)
+			displayChar.y -= moveSpeed;
+		if (FlxG.keys.pressed.DOWN)
+			displayChar.y += moveSpeed;
+		if (FlxG.keys.pressed.LEFT)
+			displayChar.x -= moveSpeed;
+		if (FlxG.keys.pressed.RIGHT)
+			displayChar.x += moveSpeed;
 	}
 
 	function updateDaHitbox()
 	{
-		var dataFile = Assets.getText('assets/data/offsets/' + charDropdown.selectedLabel + '.OFFSET');
-		daBoxData = Json.parse(dataFile)[Std.parseInt(arrayHitboxChoice.text)]; // make the 0 da choice of da user
+		var dataFile = Assets.getText('$path_offset/' + charDropdown.selectedLabel + '.json');
+		daBoxData = Json.parse(dataFile)[Std.parseInt(Std.string(arrayHitboxChoice.value))];
 		displayChar.offset.x = daBoxData.x;
 		displayChar.offset.y = daBoxData.y;
 		displayChar.width = daBoxData.width;
@@ -204,7 +306,16 @@ class AnimTestState extends FlxState
 	}
 
 	function sparrowFrames(key:String)
+		return FlxAtlasFrames.fromSparrow('$path_charFolder/$key.png', '$path_charFolder/$key.xml');
+
+	public static function arrayifyTextFile(path):Array<String>
 	{
-		return FlxAtlasFrames.fromSparrow('assets/images/$key.png', 'assets/images/$key.xml');
+		var daArray:Array<String> = Assets.getText(path).trim().split('\n');
+
+		for (i in 0...daArray.length)
+		{
+			daArray[i] = daArray[i].trim();
+		}
+		return daArray;
 	}
 }
